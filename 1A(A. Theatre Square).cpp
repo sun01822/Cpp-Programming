022659c198caaae22977c3be65ecd477467ef96d
MD. Shariar Hossain Sun <75157104+MDShariarHossainSun@users.noreply.github.com>
2022-01-13 22:16:52 +0600

    CodeForces

---
+++ 1A(A. Theatre Square).cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n, m, a;
+
+    cin >> n >> m >> a;
+
+    cout << ((m+a-1)/a)*((n+a-1)/a)<< endl;
+
+    return 0;
+}
